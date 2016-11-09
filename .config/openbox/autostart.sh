03d7db090cae89881e38d62c080292938f3fe355
Hikaru Noda <hikaru.noda9@gmail.com>
2016-11-09 11:56:31 +0200

    [openbox] change desktop background

--- .config/openbox/autostart.sh
+++ .config/openbox/autostart.sh
@@ -2,19 +2,8 @@
 # Environment variables set here are passed to the Openbox session.
 
 # Set a background color
-BG=""
-if which hsetroot >/dev/null; then
-    BG=hsetroot
-else
-    if which esetroot >/dev/null; then
-    BG=esetroot
-    else
-    if which xsetroot >/dev/null; then
-        BG=xsetroot
-    fi 
-    fi 
-fi
-test -z $BG || $BG -solid "#000000"
+xsetroot -solid "#000000"
+
 #
 # D-bus
 #if which dbus-launch >/dev/null && test -z "$DBUS_SESSION_BUS_ADDRESS"; then
